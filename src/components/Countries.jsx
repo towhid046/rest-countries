d253308a550b1b23c35dabdd38d14540629b3f33
towhid046 <towhidmorol46@gmail.com>
2024-03-11 10:53:49 +0600

    added visisted functionality

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,15 +1,17 @@
+/* eslint-disable react/jsx-key */
 import { useEffect, useState } from "react";
 import Country from "./Country";
 import "../App.css";
 
 export default function Countries() {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
 
   useEffect(() => {
     const loadData = async () => {
       const res = await fetch("https://restcountries.com/v3.1/all");
       const data = await res.json();
-      
+
       setCountries(
         data.sort((a, b) => a.name.common.localeCompare(b.name.common))
       );
@@ -17,14 +19,36 @@ export default function Countries() {
     loadData();
   }, []);
 
+  const handelMarkVisited = (country, setIsVisited2) => {
+    setVisitedCountries([...visitedCountries, country]);
+    const filteredCountries = countries.filter(
+      (c) => c.name.common !== country.name.common
+    );
+    setCountries(filteredCountries);
+  };
+
+  const countriesTitleStyle = { textAlign: "center", fontFamily: "Arial" };
+
   return (
     <>
-      <h1 style={{ textAlign: "center", fontFamily: "Arial" }}>
+      <h1 style={countriesTitleStyle}>Visited countries Lengths {visitedCountries.length}</h1>
+      <ul className="countries-container">
+        {visitedCountries.map((v) => (
+          <Country key={v.name.common} country={v} />
+        ))}
+      </ul>
+
+      <h1 style={countriesTitleStyle}>
         Length of the Countries: {countries.length}
       </h1>
+
       <div className="countries-container">
         {countries.map((country) => (
-          <Country key={country.cca3} country={country} />
+          <Country
+            key={country.cca3}
+            country={country}
+            handelMarkVisited={handelMarkVisited}
+          />
         ))}
       </div>
     </>
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from "react";
 import "../App.css";
 
-export default function Country({country}) {
+export default function Country({ country, handelMarkVisited }) {
+  const [isVisited, setIsVisited] = useState(false);
   const {
     name: { common, official },
     flags: { png, alt },
@@ -17,14 +19,22 @@ export default function Country({country}) {
     currencyName = currencies[currency].name;
   }
 
+  const handelDetailsBtn = () => {
+    setIsVisited((v) => !v);
+  };
+
   return (
-    <div className="card">
+    <div className={isVisited ? "card bg-green" : "card"}>
       <img style={{ width: "320px", height: "190px" }} src={png} alt={alt} />
       <h2>Name: {common}</h2>
       <h3>Official Name: {official}</h3>
       <h4>Continent: {continents}</h4>
       <h4>Capital: {capital} </h4>
       <h4>Currency: {currencyName} </h4>
+      <button onClick={handelDetailsBtn} className="details-btn">
+        {isVisited ? "Visited" : "Not visited"}
+      </button> <br /> <br />
+      <button className="details-btn" onClick={()=>handelMarkVisited(country)}>Mark as visited</button>
     </div>
   );
 }
